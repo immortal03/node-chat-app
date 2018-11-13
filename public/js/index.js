aac97a82abcb2fb17af811971d1955f09868b1bb
Michael Howe <ahow_pch@hotmail.com>
2018-11-13 15:19:48 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'james@gmail.com',
-    text: 'hey send email'
-  });
 });
 
 socket.on('disconnect', function () {
